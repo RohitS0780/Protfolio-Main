e22fa193a3a97e02c3243d80749ae33d215dc2fb
Rohit <rohit85.com@gmail.com>
2026-08-03 09:38:17 +0530

    contact form updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,25 +41,6 @@ $(document).ready(function () {
   });
 
   // <!-- emailjs to mail contact form data -->
-  // $("#contact-form").submit(function (event) {
-  //   emailjs.init("sirsatrohit11@gmail.com");
-
-  //   emailjs
-  //     .sendForm("contact_service", "template_contact", "#contact-form")
-  //     .then(
-  //       function (response) {
-  //         console.log("SUCCESS!", response.status, response.text);
-  //         document.getElementById("contact-form").reset();
-  //         alert("Form Submitted Successfully");
-  //       },
-  //       function (error) {
-  //         console.log("FAILED...", error);
-  //         alert("Form Submission Failed! Try Again");
-  //       },
-  //     );
-  //   event.preventDefault();
-  // });
-  // <!-- emailjs to mail contact form data -->
 });
 
 document.addEventListener("visibilitychange", function () {
